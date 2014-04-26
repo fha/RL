d9d0c84df4bb26f5a7dc65eaf2cb0356a9fa04bf
Fahad Alhasoun <fha@mit.edu>
2014-04-26 13:35:14 -0400

    delete

--- final_project_code_mac/ExampleTeam copy/config.py
+++
@@ -1,5 +0,0 @@
-teamname = 'ExampleTeam'
-
-members = [ ('Gerald Dean Rascoe', 'rascoe@college.harvard.edu'),
-            ('Edward May', 'edward@fas.harvard.edu'),
-            ('Geraldine Rascoe', 'geraldine@seas.harvard.edu')]
--- final_project_code_mac/SJFteam/config.py
+++
@@ -1,5 +0,0 @@
-teamname = 'SJFteam'
-
-members = [ ('Fahad Alhasoun', 'fha@mit.edu'),
-            ('Edward May', 'edward@fas.harvard.edu'),
-            ('Geraldine Rascoe', 'geraldine@seas.harvard.edu')]
