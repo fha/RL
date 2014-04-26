52574b98d7b5dbb0706873e56e27c1c5df0ba00d
Fahad Alhasoun <fha@mit.edu>
2014-04-26 13:25:17 -0400

    1

---
+++ final_project_code_mac/SJFteam/config.py
@@ -0,0 +1,5 @@
+teamname = 'SJFteam'
+
+members = [ ('Fahad Alhasoun', 'fha@mit.edu'),
+            ('Edward May', 'edward@fas.harvard.edu'),
+            ('Geraldine Rascoe', 'geraldine@seas.harvard.edu')]
